045edcb4cfe2218e2cc33910f73f1f1d2ddc1d20
Nathan Fielder <commit@nfielder.dev>
2023-04-19 12:38:04 +0100

    Remove unnecessary which-key shortcuts

--- home/dot_config/nvim/lua/plugins/editor.lua
+++ home/dot_config/nvim/lua/plugins/editor.lua
@@ -191,17 +191,14 @@ return {
                 local keymaps = {
                     mode = { "n", "v" },
                     ["g"] = { name = "+goto" },
-                    ["gz"] = { name = "+surround" },
                     ["]"] = { name = "+next" },
                     ["["] = { name = "+prev" },
-                    ["<leader>b"] = { name = "+buffer" },
                     ["<leader>c"] = { name = "+code" },
                     ["<leader>f"] = { name = "+file/find" },
                     ["<leader>g"] = { name = "+git" },
                     ["<leader>gh"] = { name = "+hunks" },
                     ["<leader>s"] = { name = "+search" },
                     ["<leader>u"] = { name = "+ui" },
-                    ["<leader>w"] = { name = "+windows" },
                     ["<leader>x"] = { name = "+diagnostics/quickfix" },
                 }
                 wk.register(keymaps)
