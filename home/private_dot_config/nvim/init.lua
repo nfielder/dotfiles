f73658005bcb90d7d9e5487ba32e4a78327d74b6
Nathan Fielder <commit@nfielder.dev>
2024-07-15 23:31:56 +0100

    chore(nvim): update conform.nvim

--- home/private_dot_config/nvim/init.lua
+++ home/private_dot_config/nvim/init.lua
@@ -461,16 +461,31 @@ require('lazy').setup({
 
   { -- Autoformat
     'stevearc/conform.nvim',
+    lazy = false,
+    keys = {
+      {
+        '<leader>f',
+        function()
+          require('conform').format { async = true, lsp_format = 'fallback' }
+        end,
+        mode = 'n',
+        desc = '[F]ormat buffer',
+      },
+    },
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
+        local format_mode = 'fallback'
         local disable_filetypes = { c = true, cpp = true }
+        if disable_filetypes[vim.bo[bufnr].filetype] then
+          format_mode = 'never'
+        end
         return {
           timeout_ms = 500,
-          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+          lsp_format = format_mode,
         }
       end,
       formatters_by_ft = {
@@ -481,16 +496,6 @@ require('lazy').setup({
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         -- javascript = { { "prettierd", "prettier" } },
-      },
-    },
-    keys = {
-      {
-        '<leader>cf',
-        function()
-          require('conform').format()
-        end,
-        mode = 'n',
-        desc = '[C]ode [F]ormat',
       },
     },
   },
