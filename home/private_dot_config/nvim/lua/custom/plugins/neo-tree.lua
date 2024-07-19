2a3c9fa86a6f258795fc09e27e16da945cee790a
Nathan Fielder <commit@nfielder.dev>
2024-07-19 14:26:53 +0100

    feat(nvim): add Neotree plugin

---
+++ home/private_dot_config/nvim/lua/custom/plugins/neo-tree.lua
@@ -0,0 +1,22 @@
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = 'v3.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  cmd = 'Neotree',
+  keys = {
+    { '\\', '<cmd>Neotree reveal<CR>', { desc = 'Neotree reveal' } },
+  },
+  opts = {
+    filesystem = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+  },
+}
