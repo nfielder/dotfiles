2811397f9db0a6d54d33aa6f8297cd692a3bb2de
Nathan Fielder <commit@nfielder.dev>
2024-07-22 16:42:56 +0100

    feat(nvim): set indentation options in .templ files

--- home/private_dot_config/nvim/init.lua
+++ home/private_dot_config/nvim/init.lua
@@ -906,7 +906,7 @@ vim.api.nvim_create_autocmd({ 'FileType' }, {
 
 -- Use tabs in go files
 vim.api.nvim_create_autocmd({ 'FileType' }, {
-  pattern = { 'go' },
+  pattern = { 'go', 'templ' },
   callback = function()
     vim.opt_local.expandtab = false
     vim.opt_local.shiftwidth = 4
