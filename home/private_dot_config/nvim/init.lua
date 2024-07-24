65366eead24070ea93478252393473e95162b42e
Nathan Fielder <commit@nfielder.dev>
2024-07-25 00:44:14 +0100

    feat(nvim): lazy load conform.nvim

--- home/private_dot_config/nvim/init.lua
+++ home/private_dot_config/nvim/init.lua
@@ -581,7 +581,8 @@ require('lazy').setup({
 
   { -- Autoformat
     'stevearc/conform.nvim',
-    lazy = false,
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
     keys = {
       {
         '<leader>f',
