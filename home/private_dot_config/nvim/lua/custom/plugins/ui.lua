c8b962c9d46e4e8c1ba0afd9911259442a1c42c0
Nathan Fielder <commit@nfielder.dev>
2024-07-27 01:09:04 +0100

    feat(nvim): add oil.nvim plugin

--- home/private_dot_config/nvim/lua/custom/plugins/ui.lua
+++ home/private_dot_config/nvim/lua/custom/plugins/ui.lua
@@ -7,4 +7,18 @@ return {
     main = 'ibl',
     opts = {},
   },
+  {
+    'stevearc/oil.nvim',
+    cmd = 'Oil',
+    dependencies = { 'echasnovski/mini.nvim' }, -- TODO: split out mini plugins individually
+    opts = {
+      default_file_explorer = false,
+      view_options = {
+        show_hidden = true,
+        is_always_hidden = function(name, _)
+          return name == '.git'
+        end,
+      },
+    },
+  },
 }
