b2f11d7ff971994129919428d26b68b0ea2d2daf
Nathan Fielder <commit@nfielder.dev>
2024-08-05 11:05:50 +0100

    feat(nvim): add lazydev.nvim plugin
    Replaces neodev.nvim

--- home/private_dot_config/nvim/lua/custom/plugins/lsp.lua
+++ home/private_dot_config/nvim/lua/custom/plugins/lsp.lua
@@ -1,5 +1,18 @@
 return {
-  -- LSP Configuration & Plugins
+  {
+    -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+    -- used for completion, annotations and signatures of Neovim apis
+    'folke/lazydev.nvim',
+    ft = 'lua',
+    opts = {
+      library = {
+        -- Load luvit types when the `vim.uv` word is found
+        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+      },
+    },
+  },
+  { 'Bilal2453/luvit-meta', lazy = true },
+  -- Main LSP configuration
   {
     'neovim/nvim-lspconfig',
     dependencies = {
@@ -11,10 +24,6 @@ return {
       -- Useful status updates for LSP.
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { 'j-hui/fidget.nvim', opts = {} },
-
-      -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
-      -- used for completion, annotations and signatures of Neovim apis
-      { 'folke/neodev.nvim', opts = {} },
 
       -- Allows extra capabilities provided by nvim-cmp
       'hrsh7th/cmp-nvim-lsp',
