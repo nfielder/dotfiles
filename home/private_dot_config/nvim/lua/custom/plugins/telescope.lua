25c94b708b0f38075fb4b5897624e11032d5ed30
Nathan Fielder <commit@nfielder.dev>
2024-10-18 21:42:39 +0100

    chore(nvim): reorganize file search keymaps

--- home/private_dot_config/nvim/lua/custom/plugins/telescope.lua
+++ home/private_dot_config/nvim/lua/custom/plugins/telescope.lua
@@ -113,18 +113,21 @@ return {
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>s:', builtin.commands, { desc = '[S]earch [:] available commands' })
+
+      -- File related
       vim.keymap.set('n', '<leader>sf', function()
         find_files_from_project_git_root()
       end, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>sa', function()
         find_files_from_project_git_root { no_ignore = true }
       end, { desc = '[S]earch [A]ll Files' })
+      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+      vim.keymap.set('n', '<leader>se', builtin.live_grep, { desc = '[S]earch by gr[E]p' })
+      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
