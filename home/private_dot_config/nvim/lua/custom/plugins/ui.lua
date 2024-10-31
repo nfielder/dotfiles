686be6108067b83c900dad09e667ded2c71c80ee
Nathan Fielder <commit@nfielder.dev>
2024-10-31 10:23:40 +0000

    refactor(nvim): move file explorer plugins to separate file

--- home/private_dot_config/nvim/lua/custom/plugins/ui.lua
+++ home/private_dot_config/nvim/lua/custom/plugins/ui.lua
@@ -40,20 +40,4 @@ return {
     main = 'ibl',
     opts = {},
   },
-  {
-    'stevearc/oil.nvim',
-    cmd = 'Oil',
-    dependencies = {
-      'echasnovski/mini.icons',
-    },
-    opts = {
-      default_file_explorer = false,
-      view_options = {
-        show_hidden = true,
-        is_always_hidden = function(name, _)
-          return name == '.git'
-        end,
-      },
-    },
-  },
 }
