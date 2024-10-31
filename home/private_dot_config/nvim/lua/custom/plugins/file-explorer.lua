f4f6edfb589e580a966c936e1a165a50100ef26f
Nathan Fielder <commit@nfielder.dev>
2024-10-31 10:48:56 +0000

    feat(nvim): add mini.files plugin
    Also removed using Neotree as default when opening a directory directly
    from the command line.

--- home/private_dot_config/nvim/lua/custom/plugins/file-explorer.lua
+++ home/private_dot_config/nvim/lua/custom/plugins/file-explorer.lua
@@ -1,5 +1,12 @@
 return {
   {
+    'echasnovski/mini.files',
+    dependencies = {
+      'echasnovski/mini.icons',
+    },
+    opts = {},
+  },
+  {
     'nvim-neo-tree/neo-tree.nvim',
     version = 'v3.x',
     dependencies = {
@@ -11,37 +18,6 @@ return {
     keys = {
       { '\\', '<cmd>Neotree reveal<CR>', desc = 'Neotree reveal', silent = true },
     },
-    init = function()
-      vim.api.nvim_create_autocmd('BufEnter', {
-        group = vim.api.nvim_create_augroup('RemoteFileInit', { clear = true }),
-        callback = function()
-          local f = vim.fn.expand '%:p'
-          for _, v in ipairs { 'dav', 'fetch', 'ftp', 'http', 'rcp', 'rsync', 'scp', 'sftp' } do
-            local p = v .. '://'
-            if f:sub(1, #p) == p then
-              vim.cmd [[
-              unlet g:loaded_netrw
-              unlet g:loaded_netrwPlugin
-              runtime! plugin/netrwPlugin.vim
-              silent Explore %
-            ]]
-              break
-            end
-          end
-          vim.api.nvim_clear_autocmds { group = 'RemoteFileInit' }
-        end,
-      })
-      vim.api.nvim_create_autocmd('BufEnter', {
-        group = vim.api.nvim_create_augroup('NeoTreeInit', { clear = true }),
-        callback = function()
-          local f = vim.fn.expand '%:p'
-          if vim.fn.isdirectory(f) ~= 0 then
-            vim.cmd('Neotree current dir=' .. f)
-            vim.api.nvim_clear_autocmds { group = 'NeoTreeInit' }
-          end
-        end,
-      })
-    end,
     opts = {
       event_handlers = {
         {
@@ -61,7 +37,6 @@ return {
             '.git',
           },
         },
-        hijack_netrw_behavior = 'open_current',
         window = {
           mappings = {
             ['\\'] = 'close_window',
