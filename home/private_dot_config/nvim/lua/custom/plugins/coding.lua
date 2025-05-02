663a41b58fabf586caf31053ab723f509ec1d7ae
Nathan Fielder <commit@nfielder.dev>
2025-05-03 00:17:55 +0100

    feat(nvim): remove friendly snippets plugin

--- home/private_dot_config/nvim/lua/custom/plugins/coding.lua
+++ home/private_dot_config/nvim/lua/custom/plugins/coding.lua
@@ -20,17 +20,6 @@ return {
           end
           return 'make install_jsregexp'
         end)(),
-        dependencies = {
-          -- `friendly-snippets` contains a variety of premade snippets.
-          --    See the README about individual language/framework/plugin snippets:
-          --    https://github.com/rafamadriz/friendly-snippets
-          {
-            'rafamadriz/friendly-snippets',
-            config = function()
-              require('luasnip.loaders.from_vscode').lazy_load()
-            end,
-          },
-        },
         opts = function()
           return {
             -- NOTE: This load_ft_func seems borked
