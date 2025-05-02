7ca944566d64a58079646ae0bc1805f5571d3730
Nathan Fielder <commit@nfielder.dev>
2025-05-03 00:33:30 +0100

    feat: remove telescope `0.1.x` branch lock

--- home/private_dot_config/nvim/lua/custom/plugins/telescope.lua
+++ home/private_dot_config/nvim/lua/custom/plugins/telescope.lua
@@ -3,7 +3,6 @@ return {
   {
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
-    branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
       { -- If encountering errors, see telescope-fzf-native README for installation instructions
