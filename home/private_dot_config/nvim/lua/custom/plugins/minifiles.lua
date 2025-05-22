74eb535aef9cf5c1be06eb313bab2562274e0086
Nathan Fielder <commit@nfielder.dev>
2025-05-22 23:03:50 +0100

    chore(nvim): remove neo-tree.nvim

--- home/private_dot_config/nvim/lua/custom/plugins/minifiles.lua
+++ home/private_dot_config/nvim/lua/custom/plugins/minifiles.lua
@@ -60,44 +60,4 @@ return {
       },
     },
   },
-  {
-    'nvim-neo-tree/neo-tree.nvim',
-    version = 'v3.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      { 'nvim-tree/nvim-web-devicons', cond = vim.g.have_nerd_font }, -- not strictly required, but recommended
-      'MunifTanjim/nui.nvim',
-    },
-    cmd = 'Neotree',
-    keys = {
-      { '\\', '<cmd>Neotree reveal<CR>', desc = 'Neotree reveal', silent = true },
-    },
-    opts = {
-      event_handlers = {
-        {
-          event = 'file_open_requested',
-          handler = function()
-            -- autoclose
-            require('neo-tree.command').execute { action = 'close' }
-          end,
-        },
-      },
-      filesystem = {
-        hijack_netrw_behavior = 'disabled',
-        filtered_items = {
-          visible = false,
-          hide_dotfiles = false,
-          hide_gitignored = true,
-          never_show = {
-            '.git',
-          },
-        },
-        window = {
-          mappings = {
-            ['\\'] = 'close_window',
-          },
-        },
-      },
-    },
-  },
 }
