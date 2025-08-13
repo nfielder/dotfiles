b06c8011a7f4d8888896229d4cfa8de1ba65673d
Nathan Fielder <commit@nfielder.dev>
2025-08-13 15:52:45 +0100

    chore: move lazydev to own file

--- home/private_dot_config/nvim/lua/custom/exact_plugins/lsp.lua
+++ home/private_dot_config/nvim/lua/custom/exact_plugins/lsp.lua
@@ -1,16 +1,4 @@
 return {
-  {
-    -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
-    -- used for completion, annotations and signatures of Neovim apis
-    'folke/lazydev.nvim',
-    ft = 'lua',
-    opts = {
-      library = {
-        -- Load luvit types when the `vim.uv` word is found
-        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
-      },
-    },
-  },
   -- Main LSP configuration
   {
     'neovim/nvim-lspconfig',
