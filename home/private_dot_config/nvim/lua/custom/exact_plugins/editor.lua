17b96307d64475f0075c2b34f0d809edf4cbf3b8
Nathan Fielder <commit@nfielder.dev>
2026-02-26 14:38:03 +0000

    feat(nvim): replace VimSleuth with guess-indent.nvim

--- home/private_dot_config/nvim/lua/custom/exact_plugins/editor.lua
+++ home/private_dot_config/nvim/lua/custom/exact_plugins/editor.lua
@@ -1,7 +1,7 @@
 ---@module 'lazy'
 ---@type LazySpec
 return {
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  { 'nmac427/guess-indent.nvim', opts = {} },
 
   -- See `:help gitsigns` to understand what the configuration keys do
   {
